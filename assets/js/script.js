b5a0e33649047737714dfa2b548430ac0b978c0a
sheenaanto <sheena.anto08@gmail.com>
2025-11-11 20:16:14 +0000

    Add event listeners for button clicks to confirm interactivity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
+// Wait for the Dom to finish loading before running the game
+// Get the button elements and assign the evenetlisteners to it
+
+document.addEventListener("DOMContentLoaded", function() {
+   let buttons=document.getElementsByTagName("button");
+   for(let button of buttons)
+   {
+      button.addEventListener("click",function(){
+        if(this.getAttribute(data-type)=="submit")
+            alert("You clicked submit");
+        else
+        {
+            let gameType=this.getAttribute(data-type);
+            alert(`You clicked ${gameType}`)
+        }
+      })
+    }
+  })
+
+
 function runGame(){
 
 
 }
 function checkAnswer(){
+
+
     
 
 }
