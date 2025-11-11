851181d4d501d6bc332436377cfb07addf88cba5
sheenaanto <sheena.anto08@gmail.com>
2025-11-11 15:42:30 +0000

    Add initial JavaScript functions for game logic

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+function runGame(){
+
+
+}
+function checkAnswer(){
+    
+
+}
+function calculateCorrectAnswer(){
+    
+
+}
+function incrementScore(){
+    
+
+}
+function incrementWrongAnswer(){
+    
+
+}
+function displatAdditionQuestion(){
+    
+
+}
+function displaySubtratQuestion(){
+    
+
+}
+function displayMultiplyQuestion(){
+    
+
+}
+function displayDivideQuestion(){
+    
+
+}
