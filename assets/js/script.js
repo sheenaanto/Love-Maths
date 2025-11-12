bbec92be99bdb000cc1e487301e67e8dbd842f71
sheenaanto <sheena.anto08@gmail.com>
2025-11-12 16:10:47 +0000

    Add subtraction question display logic and ensure operands are ordered correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,9 @@ function runGame(gameType){
       }
       else if (gameType==="subtract")
         {
+         
           displaySubtractQuestion(num1,num2);
+       
         }
         else if(gameType==="multiply")
           {
@@ -123,8 +125,8 @@ function displayAdditionQuestion(operand1,operand2){
 
 }
 function displaySubtractQuestion(operand1,operand2){
-    document.getElementById("operand1").textContent=operand1;
-  document.getElementById("operand2").textContent=operand2;
+    document.getElementById("operand1").textContent=operand1>operand2? operand1:operand2;
+  document.getElementById("operand2").textContent=operand1>operand2? operand2:operand1;
   document.getElementById("operator").textContent="-";
 
 }
