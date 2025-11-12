b2f441392acbe824fec06b3560070bb9bad5f775
sheenaanto <sheena.anto08@gmail.com>
2025-11-12 16:02:08 +0000

    Implement multiplication functionality in the game with corresponding display

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,10 +36,10 @@ function runGame(gameType){
         {
           displaySubtractQuestion(num1,num2);
         }
-        // else if(gameType==="multiply")
-        //   {
-        //   displayMultiplyQuestion(num1,num2);
-        // } 
+        else if(gameType==="multiply")
+          {
+          displayMultiplyQuestion(num1,num2);
+        } 
         //  else if(gameType==="division")
         //   {
         //   displayDivideQuestion(num1,num2);
@@ -86,8 +86,8 @@ function calculateCorrectAnswer(){
        return [operand1+operand2,"addition"];
       else if(operator=="-")
         return[operand1-operand2,"subtract"]
-      // else if (operator=="x")
-      //   return[operand1*operand2,"multiply"]
+      else if (operator=="x")
+        return[operand1*operand2,"multiply"]
       // else if (operator=="x")
       //   return[operand1/operand2,"division"]
     else
@@ -128,12 +128,12 @@ function displaySubtractQuestion(operand1,operand2){
   document.getElementById("operator").textContent="-";
 
 }
-// function displayMultiplyQuestion(operand1,operand2){
-//     document.getElementById("operand1").textContent=operand1;
-//   document.getElementById("operand2").textContent=operand2;
-//   document.getElementById("operator").textContent="x";
+function displayMultiplyQuestion(operand1,operand2){
+    document.getElementById("operand1").textContent=operand1;
+  document.getElementById("operand2").textContent=operand2;
+  document.getElementById("operator").textContent="x";
 
-// }
+}
 // function displayDivideQuestion(operand1,operand2){
 //     document.getElementById("operand1").textContent=operand1;
 //   document.getElementById("operand2").textContent=operand2;
